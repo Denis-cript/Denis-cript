eeb9a7e4cbd3bb284788abdbe9533ec4a18d2475
Денис <d.korzan@mail.ru>
2021-02-27 19:25:27 +0800

    Revert "Закрепление материала."

--- src/WhileLoops.java
+++
@@ -1,9 +0,0 @@
-public class WhileLoops {
-    public static void main(String[] args) {
-        int car = 21;
-        while (car<27) {
-            System.out.println("Mercedes " +car);
-            car = car+1;
-        }
-    }
-}
