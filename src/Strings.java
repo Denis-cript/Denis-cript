2fd0221ba656becc99e1fa7d86542f994115ce0f
Денис <d.korzan@mail.ru>
2021-02-27 19:16:20 +0800

    Закрепление материала.

---
+++ src/Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String[] args) {
+        String s = "eggs ";
+        String a = "chiken";
+        System.out.println(s+a);
+        System.out.println("eggs "+"chiken");
+
+    }
+}
+
